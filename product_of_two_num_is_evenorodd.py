fa48ef0663b022fa152afd51ce44788094c567e7
pvs <40256682+sudhakarpv@users.noreply.github.com>
2018-07-22 13:52:35 +0530

    Update product_of_two_num_is_evenorodd.py

--- product_of_two_num_is_evenorodd.py
+++ product_of_two_num_is_evenorodd.py
@@ -1,1 +1,13 @@
 # product_of_two_is_even
+def main():
+    pass
+    user=input().split()
+    n=user[0]
+    m=user[1]
+    k=(int(n)*int(m))
+    if (k%2==0):
+        print("even")
+    else:
+        print("odd")
+if __name__ == '__main__':
+    main()
